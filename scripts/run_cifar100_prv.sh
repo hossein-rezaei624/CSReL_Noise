e934d52840d4cd7dc29af37c8b2cfbbf029c35d0
Hossein Rezaei <hr505@cl.cam.ac.uk>
2026-05-20 00:48:08 +0330

    Update run_cifar100_prv.sh

--- scripts/run_cifar100_prv.sh
+++ scripts/run_cifar100_prv.sh
@@ -30,7 +30,7 @@ aug_type='der'
 ref_train_lr=3e-3
 cur_train_lr=5e-3
 ref_sample_per_task=200
-noise_rate=0.1
+noise_rate=0.0
 
 
 python3 -u offline_continual_learning.py --local_path=$local_path \
