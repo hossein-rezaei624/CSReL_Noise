67175bba18cfc2e38a196a4008c9bc0b2824b822
Hossein Rezaei <hr505@cl.cam.ac.uk>
2026-05-24 20:28:26 +0330

    Update idataset.py

--- dataset/idataset.py
+++ dataset/idataset.py
@@ -309,7 +309,7 @@ class SplitCifar100(object):
                 self.train_dataset,
                 noise_rate=label_noise,
                 n_classes_total=100,
-                n_classes_per_task=10,
+                n_classes_per_task=5,
                 within_task=True,
                 seed=seed,
             )
@@ -357,8 +357,8 @@ class SplitCifar100(object):
             return train_ind, full_train_id, test_ind
 
     def make_task_dic(self):
-        tasks = 10
-        cls_per_task = 10
+        tasks = 20
+        cls_per_task = 5
         cur_class = 0
         for i in range(tasks):
             self.task_dic[i] = []
