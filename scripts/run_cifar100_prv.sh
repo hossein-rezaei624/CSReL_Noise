4357c50da2d5ba4c6f9e40397102ea9c326d027b
Hossein Rezaei <hr505@cl.cam.ac.uk>
2026-07-13 19:59:21 +0330

    Update run_cifar100_prv.sh

--- scripts/run_cifar100_prv.sh
+++ scripts/run_cifar100_prv.sh
@@ -1,6 +1,6 @@
 export CUBLAS_WORKSPACE_CONFIG=:4096:8
 
-local_path='./results/split_cifar100/test124'  # set your output path
+local_path='./results/split_cifar100/test125'  # set your output path
 dataset='splitcifar100'
 setting='der'
 data_path=''
@@ -8,12 +8,12 @@ buffer_size=1000
 alpha=4.0
 beta=0.0
 lr=2e-2
-epochs=100
+epochs=10
 batch_size=32
 mem_batch_size=32
 use_cuda=1
 opt_type='sgd'
-seed=4
+seed=0
 slt_wo_aug=0
 holdout_set='sub'
 replay_mode='sub'
